c8854a93caaead6f19e20a56178e0a531850f0f3
Andras Gyomrey <lgandras@gmail.com>
2018-03-17 17:56:24 +0100

    Naked groups (pairs, trios, quartet, etc.). Doesn't pass the submission.

--- solution.py
+++ solution.py
@@ -7,8 +7,9 @@ column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
 unitlist = row_units + column_units + square_units
 
-# TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+# Update the unit list to add the new diagonal units
+diagonal_units = [[r + c for (r, c) in dlist] for dlist in [zip(rows, cols), zip(rows, reversed(cols))]]
+unitlist = unitlist + diagonal_units
 
 
 # Must be called after all units (including diagonals) are added to the unitlist
@@ -43,8 +44,53 @@ def naked_twins(values):
     and because it is simpler (since the reduce_puzzle function already calls this
     strategy repeatedly).
     """
-    # TODO: Implement this function!
-    raise NotImplementedError
+    def each_comb_helper(elements, n, chosen):
+        if len(elements) < n:
+            return
+        if 0 == n:
+            yield chosen
+        else:
+            yield from each_comb_helper(elements[1:], n - 1, chosen + [elements[0]])
+            yield from each_comb_helper(elements[1:], n, chosen)
+
+    def each_comb(elements, n):
+        return each_comb_helper(elements, n, [])
+
+    def each_naked_twin_group(unit, eboxes, elements):
+        sboxes = eboxes[elements[0]]
+        for element in elements[1:]:
+            if eboxes[element] != sboxes:
+                return
+
+        yield (unit, sboxes, elements)
+
+    def each_naked_twin(values):
+        print("")
+        display(values)
+        for unit in unitlist:
+            if unit not in diagonal_units:
+                eboxes = {element: [box for box in unit if element in values[box]] for element in cols}
+                for n in range(2, 10):
+                    group = [element for element in eboxes if n == len(eboxes[element])]
+                    if 0 < len(group):
+                        for comb in each_comb(group, n):
+                            yield from each_naked_twin_group(unit, eboxes, comb)
+
+    def exclude_from_box(values, box, elements):
+        nval = ''
+        for element in values[box]:
+            if element not in elements:
+                nval += element
+        values[box] = nval
+        return values
+
+    for unit, sboxes, elements in each_naked_twin(values):
+        for box in unit:
+            values = exclude_from_box(values, box, list(set(cols) - set(elements)) if box in sboxes else elements)
+
+    print("")
+    display(values)
+    return values
 
 
 def eliminate(values):
@@ -63,8 +109,9 @@ def eliminate(values):
     dict
         The values dictionary with the assigned values eliminated from peers
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    def eliminate_box(b):
+        return ''.join(sorted(set(values[b]) - set([values[p] for p in peers[b]])))
+    return {b: eliminate_box(b) if len(b) > 1 else values[b] for b in values}
 
 
 def only_choice(values):
@@ -87,8 +134,14 @@ def only_choice(values):
     -----
     You should be able to complete this function by copying your code from the classroom
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    def only_choice_box(b):
+        for u in units[b]:
+            for e in values[b]:
+              positions = [bb for bb in u if e in values[bb]]
+              if 1 == len(positions):
+                  return e
+        return values[b]
+    return {b: only_choice_box(b) for b in values}
 
 
 def reduce_puzzle(values):
@@ -105,8 +158,22 @@ def reduce_puzzle(values):
         The values dictionary after continued application of the constraint strategies
         no longer produces any changes, or False if the puzzle is unsolvable 
     """
-    # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        values = eliminate(values)
+        values = only_choice(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 
 def search(values):
@@ -128,8 +195,22 @@ def search(values):
     You should be able to complete this function by copying your code from the classroom
     and extending it to call the naked twins strategy.
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if not values:
+        return None
+    
+    unsolved = sorted([(len(values[b]), b) for b in values if len(values[b]) > 1])
+    
+    if [] == unsolved:
+        return values
+
+    b = unsolved[0][1]
+    for value in values[b]:
+        solution = search(dict(values, **{b: value}))
+        if solution:
+            return solution
 
 
 def solve(grid):
